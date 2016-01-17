33ff10e66944745e991bb82ed7e8244c1cb49306
bachtsui <dbtsui2@gmail.com>
2016-01-16 17:11:45 -0800

    Added routes for User to signup and create

--- trial2/blog2/config/routes.rb
+++ trial2/blog2/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "users#index"
 
   get "/users", to: "users#index", as: "users"
+  get "/signup", to: "users#new", as: "new_user"
+  post "/users", to: "users##create" as: "create_user"
 end
