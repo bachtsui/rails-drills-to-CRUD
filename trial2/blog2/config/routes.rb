1bfee1fa5af3cf28f75533824e0da76070da653e
bachtsui <dbtsui2@gmail.com>
2016-01-17 13:59:47 -0800

    Added routes for logging in and logging out

--- trial2/blog2/config/routes.rb
+++ trial2/blog2/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root "users#index"
+  root to: "users#index"
 
   get "/users", to: "users#index", as: "users"
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create", as: "create_user"
+
+  #Routes for login form, logging in and logging out
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 end
