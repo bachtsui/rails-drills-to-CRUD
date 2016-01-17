43aea59ce762952f402d4a5ab2c64d5f8173f7e1
bachtsui <dbtsui2@gmail.com>
2016-01-17 12:16:52 -0800

    Created a create method for Users

--- trial2/blog2/app/controllers/users_controller.rb
+++ trial2/blog2/app/controllers/users_controller.rb
@@ -9,6 +9,21 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		user = User.new(user_params)
+		#Not an instance variable?
+		if user.save
+			session[:user_id] = user.id 
+			redirect_to "/"
+		else
+			redirect_to '/signup'
+			#can also do new_users_path
+		end
 	end
-	
+
+	private
+
+		def user_params
+			params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+			#:password and :password_confirmation doesn't exist yet I don't think
+		end
 end
