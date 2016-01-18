354d349b2a35b186da6854cbbc1a409cbc9a1e38
bachtsui <dbtsui2@gmail.com>
2016-01-18 11:55:07 -0800

    Fixed error in routes, now has correct path, moving onto patch

--- trial2/blog2/config/routes.rb
+++ trial2/blog2/config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get "/users", to: "users#index", as: "users"
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create", as: "create_user"
-  get "/users/:id", to: "users#show", as: "show_user"
+  get "/users/:id", to: "users#show", as: "user"
   get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "creatures#update"
 
   #Routes for login form, logging in and logging out
   get "/login", to: "sessions#new"
